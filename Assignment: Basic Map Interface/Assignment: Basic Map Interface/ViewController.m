4401b938ac4a92708a77dcb9d00fa4a8906d025c
Taro Minowa <higepon@gmail.com>
2016-01-29 06:22:53 -0800

    Basic annotations.

--- Assignment: Basic Map Interface/Assignment: Basic Map Interface/ViewController.m
+++ Assignment: Basic Map Interface/Assignment: Basic Map Interface/ViewController.m
@@ -6,9 +6,15 @@
 //  Copyright © 2016 Higepon Taro Minowa. All rights reserved.
 //
 
+#import "MapKit/MapKit.h"
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) MKPointAnnotation *tokyoAnno;
+@property (strong, nonatomic) MKPointAnnotation *londonAnno;
+@property (strong, nonatomic) MKPointAnnotation *sanFranciscoAnno;
 
 @end
 
@@ -16,12 +22,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self addAnnotations];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)addAnnotations {
+    self.tokyoAnno = [[MKPointAnnotation alloc] init];
+    self.tokyoAnno.coordinate = CLLocationCoordinate2DMake(35.6735408, 139.570303);
+    self.tokyoAnno.title = @"Tokyo";
+
+    self.londonAnno = [[MKPointAnnotation alloc] init];
+    self.londonAnno.coordinate = CLLocationCoordinate2DMake(51.5287718,-0.2416816);
+    self.londonAnno.title = @"London";
+
+    self.sanFranciscoAnno = [[MKPointAnnotation alloc] init];
+    self.sanFranciscoAnno.coordinate = CLLocationCoordinate2DMake(37.757815,-122.5076401);
+    self.sanFranciscoAnno.title = @"San Franciso";
+
+    [self.mapView addAnnotations:@[self.tokyoAnno, self.londonAnno, self.sanFranciscoAnno]];
+}
+
+- (IBAction)tokyoTapped:(id)sender {
+    [self centerMap:self.tokyoAnno];
+}
+
+- (IBAction)londonTapped:(id)sender {
+    [self centerMap:self.londonAnno];
+}
+
+- (IBAction)sanFranciscoTapped:(id)sender {
+    [self centerMap:self.sanFranciscoAnno];
+}
+
+- (void)centerMap:(MKPointAnnotation *)centerPoint {
+    [self.mapView setCenterCoordinate:centerPoint.coordinate animated:YES];
 }
 
 @end
