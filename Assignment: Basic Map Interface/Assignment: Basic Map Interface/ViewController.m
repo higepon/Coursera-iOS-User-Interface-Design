ca55d3089cb288ccb592cde23c27ce5ce9a5b345
Taro Minowa <higepon@gmail.com>
2016-01-29 06:55:43 -0800

    User locations.

--- Assignment: Basic Map Interface/Assignment: Basic Map Interface/ViewController.m
+++ Assignment: Basic Map Interface/Assignment: Basic Map Interface/ViewController.m
@@ -9,12 +9,15 @@
 #import "MapKit/MapKit.h"
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UISwitch *switchField;
 @property (strong, nonatomic) MKPointAnnotation *tokyoAnno;
 @property (strong, nonatomic) MKPointAnnotation *londonAnno;
 @property (strong, nonatomic) MKPointAnnotation *sanFranciscoAnno;
+@property (strong, nonatomic) MKPointAnnotation *currentAnno;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -23,8 +26,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
     [self addAnnotations];
-
 }
 
 - (void)addAnnotations {
@@ -33,14 +38,33 @@
     self.tokyoAnno.title = @"Tokyo";
 
     self.londonAnno = [[MKPointAnnotation alloc] init];
-    self.londonAnno.coordinate = CLLocationCoordinate2DMake(51.5287718,-0.2416816);
+    self.londonAnno.coordinate = CLLocationCoordinate2DMake(51.5287718, -0.2416816);
     self.londonAnno.title = @"London";
 
     self.sanFranciscoAnno = [[MKPointAnnotation alloc] init];
-    self.sanFranciscoAnno.coordinate = CLLocationCoordinate2DMake(37.757815,-122.5076401);
+    self.sanFranciscoAnno.coordinate = CLLocationCoordinate2DMake(37.757815, -122.5076401);
     self.sanFranciscoAnno.title = @"San Franciso";
 
+    self.currentAnno = [[MKPointAnnotation alloc] init];
+    self.currentAnno.coordinate = CLLocationCoordinate2DMake(0.0, 0.0);
+    self.currentAnno.title = @"Current";
+
     [self.mapView addAnnotations:@[self.tokyoAnno, self.londonAnno, self.sanFranciscoAnno]];
+}
+
+- (IBAction)switchChanged:(id)sender {
+    if (self.switchField.isOn) {
+        self.mapView.showsUserLocation = YES;
+        [self.locationManager startUpdatingLocation];
+    } else {
+        self.mapView.showsUserLocation = NO;
+        [self.locationManager stopUpdatingLocation];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    self.currentAnno.coordinate = locations.lastObject.coordinate;
+    [self centerMap:self.currentAnno];
 }
 
 - (IBAction)tokyoTapped:(id)sender {
