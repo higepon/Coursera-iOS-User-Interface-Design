a2eacb709021e62c8a047ee7691543a7c49cb740
Taro Minowa <higepon@gmail.com>
2016-01-28 21:05:08 -0800

    New assignment.

---
+++ Assignment: Basic Map Interface/Assignment: Basic Map Interface/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment: Basic Map Interface
+//
+//  Created by Taro Minowa on 1/28/16.
+//  Copyright © 2016 Higepon Taro Minowa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Assignment: Basic Map Interface/Assignment: Basic Map Interface/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Assignment: Basic Map Interface
+//
+//  Created by Taro Minowa on 1/28/16.
+//  Copyright © 2016 Higepon Taro Minowa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
